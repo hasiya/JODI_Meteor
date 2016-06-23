7d154b5845abb7fdc236d0dc3378d4eb55bf3710
hasiya <hasith002@yahoo.com>
2016-06-23 17:11:59 +0100

    Working on CSV Panel - Data configaring (trying to work with any csv data)

--- client/javascript/tab_csv.js
+++ client/javascript/tab_csv.js
@@ -1,6 +1,34 @@
 var CSV_keys;
 var CSV_Data;
 var editor;
+var headerValues = [];
+var headerOrinal = "orinialVal";
+var headerPresent = "presentVal";
+var headerType = "type";
+var headerValCount = "valCount";
+
+// Return array of string values, or NULL if CSV string not well formed.
+function CSVtoArray(text) {
+    var re_valid;
+    re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+    var re_value;
+    re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+    // Return NULL if input string is not well formed CSV string.
+    if (!re_valid.test(text)) return null;
+    var a = [];                     // Initialize array to receive values.
+    text.replace(re_value, // "Walk" the string using replace with callback.
+        function (m0, m1, m2, m3) {
+            // Remove backslash from \' in single quoted values.
+            if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+            // Remove backslash from \" in double quoted values.
+            else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+            else if (m3 !== undefined) a.push(m3);
+            return ''; // Return empty string.
+        });
+    // Handle special case of empty last value.
+    if (/,\s*$/.test(text)) a.push('');
+    return a;
+}
 
 function linesMatch(lines) {
 
@@ -10,53 +38,105 @@ function linesMatch(lines) {
     };
     // var linesMatch = true;
     var lineNum = 1;
+    var headers = CSVtoArray(lines[0]);
+
     lines.forEach(function (l) {
-        if(l.split(',').length != lines[0].split(',').length){
-            lineMatch["lineMatch"] = false;
-            lineMatch["lineNum"] = lineNum;
-            return lineMatch;
+        if(l) {
+            var colNum = CSVtoArray(l);
+            if (colNum) {
+                if (colNum.length != headers.length) {
+                    lineMatch["lineMatch"] = false;
+                    lineMatch["lineNum"] = lineNum;
+                    return lineMatch;
+                }
+            }
+            else {
+                lineMatch["lineMatch"] = false;
+                lineMatch["lineNum"] = lineNum;
+                return lineMatch;
+            }
         }
         lineNum++;
     });
     return lineMatch;
 }
-function getkeys() {
+function CreatePanel() {
     var panel = document.getElementById("panel");
     panel.style.display = "inline";
-    
+
     var innerHtml = "";
     for(i=0; i<CSV_keys.length;i++){
+        var keyItem = {
+            "orinialVal": CSV_keys[i],
+            "presentVal": CSV_keys[i],
+            "type": "",
+            "valCount":{}
+        };
+
         innerHtml +=
-            "<div class='row top-buffer'>" +
-                "<div class='col-md-4 col-md-offset-1'>"+
+            "<div class='row top-buffer' id='row_"+CSV_keys[i]+"'>" +
+            "<div class='col-md-1'>" +
+            "</div>" +
+            "<div class='col-md-4'>"+
                     "<form class='form-inline'>" +
-                        "<fieldset class='form-group'>" +
-                            "<input type='text' readonly id='"+CSV_keys[i]+"' class='form-control' value='"+CSV_keys[i]+"'>" +
-                        "</fieldset>" +
-                        "<button type='button' id='"+CSV_keys[i]+"' class='csvHeaderEdit btn btn-default btn-sm'>"+
+                        "<input type='text' index='"+i+"' name='headerText' headerID='"+CSV_keys[i]+"' style='width: 80%' readonly id='header_"+CSV_keys[i]+"' class='form-control input' autocomplete='off' value='"+CSV_keys[i]+"'>" +
+                        "<button type='button' id='editBtn_"+CSV_keys[i]+"' name='headerEditBtn' headerID='"+CSV_keys[i]+"' style='width: 15%'class='pull-right csvHeaderEdit btn btn-default btn-sm'>"+
                             "<span class='glyphicon glyphicon-edit'></span>"+
                         "</button>"+
                     "</form>"+
                 "</div>" +
                 "<div class='col-md-4'>" +
-                    "<select id='selectbasicTypeTour' name='selectbasicTypeTour' class='form-control'>"+
+                    "<select id='type_"+CSV_keys[i]+"' headerID='"+CSV_keys[i]+"' index='"+i+"' name='headerType' class='csvHeaderType form-control'>"+
                         "<option value='string'>String</option>"+
                         "<option value='number'>Number</option>"+
-                        "<option value='string'>Lon</option>"+
-                        "<option value='string'>Lat</option>"+
+                        "<option value='longitude'>longitude</option>"+
+                        "<option value='latitude'>Latitude</option>"+
                     "</select>" +
                 "</div>" +
-                "<div class='col-md-4'>" +
-                    "" +
+                "<div class='col-md-3'>" +
+                "<div class='col-md-6'>" +
+                    // "<form class='form-inline'>" +
+                    "<div class='checkbox' style='width: 50%'>" +
+                        "<label for='checkBox_"+CSV_keys[i]+"'><input type='checkbox' name='headerCheck' id='checkBox_"+CSV_keys[i]+"'/>Count</label>" +
+                    "</div>" +
+                "</div>" +
+                "<div class='col-md-6'>" +
+                    "<button  type='button' id='remvBtn_"+CSV_keys[i]+"' headerID='"+CSV_keys[i]+"' name='headerremvbtn' index='"+i+"' class='pull-left csvHeaderDelete btn btn-danger btn-sm'>"+
+                        "<span class='glyphicon glyphicon-remove'></span>"+
+                    "</button>" +
+                "</div>"+
+                    // "</form> " +
+
                 "</div>" +
             "</div>";
+        headerValues.push(keyItem);
     }
     var button =
         "<div class='pull-right btn-toolbar'>" +
-            "<button class='pull-right btn-primary btn PanelDone' >Done</button>" +
-        " </div>";
+            "<button class='pull-right btn-success btn PanelDone'>Done</button>" +
+        "</div>";
     innerHtml +=button;
     panel.innerHTML= innerHtml;
+}
+
+function countValues(column) {
+    var columnVals = [];
+    var counts = {};
+
+    CSV_Data.forEach(function (d) {
+        var columnVal = d[column];
+        columnVals.push(columnVal);
+    });
+    //
+    // for (var i = 0; i < columnVals.length; i++) {
+    //     counts[columnVals[i]] = 1 + (counts[columnVals[i]] || 0);
+    // }
+
+    columnVals.forEach(function (v) {
+        counts[v] = 1 + (counts[v] || 0);
+    });
+
+    return counts
 }
 
 
@@ -78,7 +158,6 @@ Template.tab_csv.helpers({
             mode:"Plain Text",
             placeholder:"Paste your CSV file content or drag and drop the file here..."
         }
-
     },
     
     getKey: function () {
@@ -158,10 +237,10 @@ Template.tab_csv.events({
                         'data':text
                     },
                     success: function(data){
-                        console.log(data);
                         CSV_Data = data;
+                        console.log(CSV_Data);
                         CSV_keys = Object.keys(data[0]);
-                        getkeys();
+                        CreatePanel();
                         var Message = $("#message");
                         Message.html("");
                     }
@@ -177,6 +256,12 @@ Template.tab_csv.events({
     "click .ClearCSV":function (e,t) {
         editor.setValue("");
 
+        var temp;
+        CSV_keys = temp;
+        CSV_Data = temp;
+        editor = temp;
+        headerValues = [];
+
         var panel = document.getElementById("panel");
         panel.style.display = "none";
         panel.innerHTML = "";
@@ -185,15 +270,145 @@ Template.tab_csv.events({
     },
 
     "click .csvHeaderEdit":function (e) {
+        var headerIndex;
         var elem = e.currentTarget;
-        var id = elem.id;
-        var textbox = document.getElementById(id);
+        var id = elem.getAttribute("headerID");
+        var textbox = document.getElementById("header_"+id);
+        // var selection = document.getElementById("type_"+id);
         if(textbox.readOnly) {
-            elem.innerHTML = "<span class='glyphicon glyphicon-ok'></span>"
+            elem.innerHTML = "<span class='glyphicon glyphicon-ok'></span>";
+            elem.className = "pull-right csvHeaderEdit btn btn-success btn-sm";
             textbox.readOnly = false;
         }else {
-            elem.innerHTML = "<span class='glyphicon glyphicon-edit'></span>"
+            headerIndex  = textbox.getAttribute('index');
+            var header = headerValues[headerIndex];
+            header[headerPresent] = textbox.value;
+            //
+            // header[headerType] = selection.value;
+            headerValues[headerIndex]=header;
+
+            elem.innerHTML = "<span class='glyphicon glyphicon-edit'></span>";
+            elem.className = "pull-right csvHeaderEdit btn btn-default btn-sm";
             textbox.readOnly = true;
         }
+    },
+
+    "change .csvHeaderType":function (e) {
+        var elem = e.currentTarget;
+        var headerId = elem.getAttribute("headerID");
+        var headerIndex = elem.getAttribute("index");
+
+        var header = headerValues[headerIndex];
+        header[headerType] = elem.value;
+        headerValues[headerIndex] = header;
+    },
+    "click .csvHeaderDelete":function (e) {
+        var elem = e.currentTarget;
+        var panel = document.getElementById("panel");
+        var rowID = elem.getAttribute("headerID");
+        var rowElem = document.getElementById("row_"+rowID);
+        var headerIndex = elem.getAttribute("index");
+
+        headerValues.splice(headerIndex,1);
+        panel.removeChild(rowElem);
+    },
+
+
+    "click .PanelDone":function (e) {
+        headerValues.forEach(function (h) {
+            var header = h;
+            var text = document.getElementById("header_"+h[headerOrinal]);
+            var selection = document.getElementById("type_"+h[headerOrinal]);
+            var checkbox = document.getElementById("checkBox_"+h[headerOrinal]);
+            header[headerPresent] = text.value;
+            header[headerType] = selection.value;
+
+            if(checkbox.checked){
+                header[headerValCount] = countValues(h[headerOrinal])
+            }
+
+        });
+
+        CSV_Data.forEach(function (data) {
+           headerValues.forEach(function (h) {
+               if(h[headerType] == "string"){
+                    data[h[headerOrinal]] = String(data[h[headerOrinal]]);
+               }
+               else if(h[headerType] == "number"){
+                   data[h[headerOrinal]] = parseInt(data[h[headerOrinal]]);
+               }
+               else if(h[headerType] == "longitude"){
+                   data[h[headerOrinal]] = parseFloat(data[h[headerOrinal]]);
+               }
+
+               else if(h[headerType] == "latitude"){
+                   data[h[headerOrinal]] = parseFloat(data[h[headerOrinal]]);
+               }
+
+           })
+        });
+
+        var textElems = document.getElementsByName("headerText");
+        var editBtnElems = document.getElementsByName("headerEditBtn");
+        var selectElems = document.getElementsByName("headerType");
+        var checkElem = document.getElementsByName("headerCheck");
+        var remvBtnElems = document.getElementsByName("headerremvbtn");
+
+        Array.from(textElems).forEach(function (e) {
+            e.readOnly = true;
+        });
+        Array.from(editBtnElems).forEach(function (e) {
+            e.style.display = "none";
+        });
+        Array.from(selectElems).forEach(function (e) {
+            e.disabled = true;
+        });
+        Array.from(checkElem).forEach(function (e) {
+            e.disabled = true;
+        });
+        Array.from(remvBtnElems).forEach(function (e) {
+            e.style.display = "none";
+        });
+
+        document.getElementById("charts").style.display = "inline";
+
+        var elem = e.currentTarget;
+        elem.className = "pull-right btn-primary btn PanelEdit";
+        elem.innerHTML = "Edit";
+
+        console.log(CSV_Data);
+        console.log(headerValues);
+    },
+    "click .PanelEdit": function (e) {
+        var textElems = document.getElementsByName("headerText");
+        var editBtnElems = document.getElementsByName("headerEditBtn");
+        var selectElems = document.getElementsByName("headerType");
+        var checkElem = document.getElementsByName("headerCheck");
+        var remvBtnElems = document.getElementsByName("headerremvbtn");
+
+        Array.from(textElems).forEach(function (e) {
+            e.readOnly = true;
+        });
+        Array.from(editBtnElems).forEach(function (e) {
+            e.innerHTML = "<span class='glyphicon glyphicon-edit'></span>";
+            e.className = "pull-right csvHeaderEdit btn btn-default btn-sm";
+            e.style.display = "inline";
+        });
+        Array.from(selectElems).forEach(function (e) {
+            e.disabled = false;
+        });
+        Array.from(checkElem).forEach(function (e) {
+            e.disabled = false;
+        });
+        Array.from(remvBtnElems).forEach(function (e) {
+            e.style.display = "inline";
+        });
+
+        document.getElementById("charts").style.display = "none";
+
+        var elem = e.currentTarget;
+        elem.className = "pull-right btn-success btn PanelDone";
+        elem.innerHTML = "Done";
     }
+
 });
