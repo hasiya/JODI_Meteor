311cd92b7b854b2dcf9b4344113d9b7537f379bb
hasiya <hasith002@yahoo.com>
2016-07-14 17:05:15 +0100

    Working on Maps. Have to fix some things such as re-loading map (and map multi-threading), map and chart legend.

--- imports/mapbox.js
+++ imports/mapbox.js
@@ -15,11 +15,13 @@ var colours;
 // var projection
 var svg, path, g;
 var map, dots;
+var headers_colors = [];
 
 
 mapbox = function (dataObj, countHeader, lon, lat) {
 
     // Mapbox.load();
+
 
     data = dataObj;
     count_h = countHeader;
@@ -29,22 +31,28 @@ mapbox = function (dataObj, countHeader, lon, lat) {
         return d[count_h];
     }));
 
+    var colors = getcolours(headerVals.length);
+
+    var c = 0;
+    headerVals.forEach(function (h) {
+        headers_colors.push({
+            header: h,
+            color: colors[c]
+        });
+        c++;
+    });
+
+
+
     if (lat_h && lon_h) {
         data.forEach(function (d) {
             d.location = {
-                lat: d[lat_h],
-                lon: d[lon_h]
+                lat: +d[lat_h],
+                lon: +d[lon_h]
             };
         });
     }
-    if (lat_h && lon_h) {
-        data.forEach(function (d) {
-            d.location = {
-                lat: d[lat_h],
-                lon: d[lon_h]
-            };
-        });
-    }
+
     // if (count_h) {
     //     count_data = d3.nest()
     //         .key(function (d) {
@@ -59,44 +67,70 @@ mapbox = function (dataObj, countHeader, lon, lat) {
     // svgDiv.style.width = width;
     svgDiv.style.height = height;
 
-    // if (Mapbox.loaded()) {
-    //     L.mapbox.accessToken = TOKEN;
-    //     // map = L.mapbox.map('svgMap', 'mapbox.streets')
-    //     //     .setView([55, -2], 5);
-    //     map = L.map('svgMap','mapbox.streets')
-    //         .setView([55, -2], 5);
-    //
-    // }
-
     mapboxgl.accessToken = "pk.eyJ1IjoiaGFzaXlhIiwiYSI6ImNpcWprZHphODBhbGhmbm5lZDJ4cXM0aDQifQ.QGJsxbUoJ_puG6JliOILiQ";
 
     map = new mapboxgl.Map({
         container: 'svgMap', // container id
-        style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
-        center: [-2, 55], // starting position
-        zoom: 4 // starting zoom
+        style: 'mapbox://styles/mapbox/streets-v9' //stylesheet location
     });
 
     // map.scrollZoom.disable()
     map.dragRotate.disable();
-    // map.addControl(new mapboxgl.Navigation({
-    //     position: 'top-left'
-    // }));
-
-    // L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGFzaXlhIiwiYSI6ImNpcWprZHphODBhbGhmbm5lZDJ4cXM0aDQifQ.QGJsxbUoJ_puG6JliOILiQ').addTo(map);
-    //
-    //
-    // svg =d3.select(map.getPanes().overlayPane).append("svg");
-    // mapLocations();
 
     var container = map.getCanvasContainer();
     svg = d3.select(container).append('svg');
     svg.attr('class', 'map');
 
+    var locationList = [];
+
+    data.forEach(function (d) {
+        locationList.push(d.location)
+    });
+
+    // var dataLocMidpoint = getLocationMidpoint(locationList);
+    var dataLocMidpoint = avgLoc(locationList);
+    map.setCenter([dataLocMidpoint.lon, dataLocMidpoint.lat]);
+    var mapBounds = getBounds(locationList);
+    map.fitBounds(mapBounds);
+
     mapLocations();
     legend();
 
 };
+
+function getcolours(count) {
+    var i = 0;
+    var colours = [];
+
+    while (i < count) {
+        var color = randomColor({
+            luminosity: 'bright'
+        });
+        while (colours.includes(color)) {
+            color = randomColor({
+                luminosity: 'bright'
+            });
+
+        }
+        colours.push(color);
+
+
+        i++
+    }
+
+    return colours;
+}
+
+function getHeaderColor(header) {
+    var h_color;
+    headers_colors.forEach(function (hc) {
+        if (hc.header == header) {
+            h_color = hc.color;
+        }
+    });
+
+    return h_color;
+}
 
 function compute_size() {
     var margin = 50;
@@ -128,7 +162,7 @@ function mapLocations() {
     dots.enter().append("circle").classed('dot', true)
         .attr("r", 1)
         .style("fill", function (d) {
-            return colours(d[count_h]);
+            return getHeaderColor(d[count_h]);
         })
         .style("fill-opacity", "2")
         .transition().duration(1000)
@@ -186,28 +220,6 @@ function mapLocations() {
 }
 
 function legend() {
-    // var legend = svg.selectAll(".legend")
-    //     .data(headerVals.slice().reverse())
-    //     .enter().append("g")
-    //     .attr("class", "legend")
-    //     .attr("transform", function (d, i) {
-    //         return "translate(0," + i * 20 + ")";
-    //     });
-    //
-    // legend.append("rect")
-    //     .attr("x", width - 7)
-    //     .attr("width", 18)
-    //     .attr("height", 18)
-    //     .style("fill", colours);
-    //
-    // legend.append("text")
-    //     .attr("x", width - 13)
-    //     .attr("y", 9)
-    //     .attr("dy", ".35em")
-    //     .style("text-anchor", "end")
-    //     .text(function (d) {
-    //         return d;
-    //     });
 
     var chartbody = d3.select('#chartBody');
 
@@ -236,7 +248,9 @@ function legend() {
         .attr("x", 5)
         .attr("width", 18)
         .attr("height", 18)
-        .style("fill", colours);
+        .style("fill", function (d) {
+            return getHeaderColor(d);
+        });
 
     legend.append("text")
         .attr("x", 30)
@@ -246,7 +260,6 @@ function legend() {
         .text(function (d) {
             return d;
         });
-
 }
 
 function project(d) {
@@ -262,4 +275,113 @@ function render() {
             return project(d).y
         }
     });
+}
+
+function getLocationMidpoint(locations) {
+    var cartesianCoords = [];
+    var latLon_r = [];
+    var X = 0, Y = 0, Z = 0;
+    locations.forEach(function (l) {
+        var lat_r = l.lat * Math.PI / 180;
+        var lon_r = l.lon * Math.PI / 180;
+
+        latLon_r.push({
+            lat_r: lat_r,
+            lon_r: lon_r
+        });
+
+        var x, y, z;
+
+        x = Math.cos(lat_r) * Math.cos(lon_r);
+        y = Math.cos(lat_r) * Math.sin(lon_r);
+        z = Math.sin(lat_r);
+
+        var coords = {
+            x: x,
+            y: y,
+            z: z
+        };
+
+        cartesianCoords.push(coords);
+    });
+
+    var totWeight = locations.length;
+
+    cartesianCoords.forEach(function (c) {
+        X += (c.x);
+        Z += (c.z);
+        Y += (c.x);
+    });
+
+    X = X / totWeight;
+    Y = Y / totWeight;
+    Z = Z / totWeight;
+
+    var Lon = Math.atan2(X, Y);
+    var Hyp = Math.sqrt(X * X + Y * Y);
+    var Lat = Math.atan2(Hyp, Z);
+
+    var center_lat = Lat * 180 / Math.PI;
+    var center_lon = Lon * 180 / Math.PI;
+
+    var lat = 0, lon = 0;
+    latLon_r.forEach(function (l) {
+        lat += l.lat_r;
+        lon += l.lon_r;
+    });
+
+    lat = lat / totWeight;
+    lon = lon / totWeight;
+
+    if (lon < -180) {
+        lon = lon - 360;
+    }
+    if (lon > 180) {
+        lon = lon + 360;
+    }
+    //
+    // return {
+    //     lat: center_lat,
+    //     lon: center_lon
+    // };
+
+    return {
+        lat: lat,
+        lon: lon
+    };
+}
+
+function avgLoc(locations) {
+    var totLon = 0, totLat = 0;
+
+    locations.forEach(function (l) {
+        totLat += l.lat;
+        totLon += l.lon;
+    });
+
+    var latAvg = totLat / locations.length;
+    var lonAvg = totLon / locations.length;
+
+    return {
+        lat: latAvg,
+        lon: lonAvg
+    };
+
+}
+
+function getBounds(locations) {
+    var lons = [];
+    var lats = [];
+
+    locations.forEach(function (l) {
+        lats.push(l.lat);
+        lons.push(l.lon);
+    });
+    var maxLat = Math.max(...lats);
+    var maxLon = Math.max(...lons);
+    var minLat = Math.min(...lats);
+    var minLon = Math.min(...lons);
+
+    var margine = 0.019;
+    return [[minLon - margine, minLat - margine], [maxLon + margine, maxLat + margine]];
 }
